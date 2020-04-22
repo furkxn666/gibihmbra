b69ce481bc2f05d9be753a9cf0a64141b3b30174
Glitch (periwinkle-recorder) <none>
2020-04-22 06:22:39 +0000

    🏪🐮 Checkpoint ./komutlar/davet.js:13562841/36

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
     .setColor("RANDOM")
     .setAuthor(`${client.user.username} İletişim Bilgileri`)
     .addField('BİZ KİMİZ?', 'Discord kullanıcıların sunucularını daha iyi hale getirmek için çabalayan bir ekibiz.')//ne kadar yetkili komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
-    .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=650739604789395476&scope=bot&permissions=805314622
+    .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=702221500035498025&scope=bot&permissions=805314622
 ) **|** [Destek Sunucusu](https://discord.gg/AHe4u4m) **|** [Bota Oy Ver (Vote)](https://discordbots.org/bot/${botid}/vote) **|** [Web Sitesi ( Yakında )]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
     message.channel.sendEmbed(embed);
 
