fdabf3cae60235a56cc567098824c8e6077fbbc9
Glitch (escape-from-others) <none>
2020-05-10 07:13:35 +0000

    🦀🚊 Checkpoint ./bot.js:13533294/341

--- bot.js
+++ bot.js
@@ -984,20 +984,6 @@ client.on('message', msg => {
 
 
 
-const express = require("express");
-const http = require("http");
-const app = express();
- 
-app.get("/", (request, response) => {
-  //console.log(Date.now() + " BOT Aktif.");
-  //response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me`);
-}, 1000 * 60 * 3);
-
-
 
 client.on("guildMemberAdd", member => {
 const embed = new Discord.RichEmbed()
