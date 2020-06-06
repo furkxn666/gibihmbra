02d8c75cf2d9d8cd278d2d283ab6577584bddfff
Glitch (escape-from-others) <none>
2020-06-06 08:42:15 +0000

    🏏😽 Checkpoint ./events/ready.js:13533294/158

--- events/ready.js
+++ events/ready.js
@@ -17,12 +17,16 @@ var oyun = [
 
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
 
-        client.user.setActivity(oyun[random], "https://youtube.com/mrjackerpvpkral" );
+        client.user.setActivity(oyun[random], "https://youtube.com/Erenarslanyeah" );
         }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
+  client.user.setStatus("online"); 
   client.user.setActivity(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
 };
+
+//dnd = Rahatsız Etmeyin
+//online = Çevrimiçi
+//idle = Boşta
