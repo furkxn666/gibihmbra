019c77b6d04d0d2e6d10d1b62db50d530dfa2110
Glitch (escape-from-others) <none>
2020-07-23 12:39:16 +0000

    🎥🏵 Checkpoint ./bot.js:19652163/5816 ./ayarlar.json:19652163/259 ./komutlar/başvuru.js:19652163/184

--- bot.js
+++ bot.js
@@ -1,24 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+    app.get("/",(requesst, response) => {
+      console.log(`pingleme işlemi başarılı başarılıysa bu yazıyı loglarda görürsün`);
+      response.sendStatus(200);
+    });
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const db = require('quick.db')
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const ayarlar = require("./ayarlar.json");
-const chalk = require('chalk');
-const fs = require('fs');
-const ms = require('parse-ms')
-const Canvas = require('canvas')
-const instagram = require("user-instagram");
-const moment = require('moment');
-require('./util/eventLoader')(client);
+const chalk = require("chalk");
+const fs = require("fs");
+const moment = require("moment");
+require("./util/eventLoader")(client);
 
 var prefix = ayarlar.prefix;
 
 const log = message => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
 };
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
+fs.readdir("./komutlar/", (err, files) => {
   if (err) console.error(err);
   log(`${files.length} komut yüklenecek.`);
   files.forEach(f => {
@@ -45,11 +54,12 @@ client.reload = command => {
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
-    } catch (e){
+    } catch (e) {
       reject(e);
     }
   });
 };
+
 
 client.load = command => {
   return new Promise((resolve, reject) => {
@@ -60,7 +70,7 @@ client.load = command => {
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
-    } catch (e){
+    } catch (e) {
       reject(e);
     }
   });
@@ -76,11 +86,35 @@ client.unload = command => {
         if (cmd === command) client.aliases.delete(alias);
       });
       resolve();
-    } catch (e){
+    } catch (e) {
       reject(e);
     }
   });
+  };
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
 };
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
